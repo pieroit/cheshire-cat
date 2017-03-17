f5accbdf983cce1171a655b3ec8e77bf88228ee5
pieroit <piero.savastano@gmail.com>
2017-03-17 13:07:34 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var React        = require('react')
+var ReactDOM     = require('react-dom')
+var Redux        = require('redux')
+var ReactRedux   = require('react-redux')
+var reduxReducer = require('./reducer')
+var App          = require('./components/App')
+
+var launchApp = function() {
+
+    // initial state
+    var appState = {}
+
+    var store = Redux.createStore(reduxReducer, appState)
+
+    ReactDOM.render(
+        <ReactRedux.Provider store={ store } >
+            <App />
+        </ReactRedux.Provider>,
+        document.getElementById('app')
+    )
+}
+
+launchApp()
