a508263093d9fbc7c45ba40b628c3f7cc27b11e6
pieroit <piero.savastano@gmail.com>
2017-05-23 11:41:42 +0200

    hide redux into the cheshire cat

--- js/components/CheshireCat.js
+++ js/components/CheshireCat.js
@@ -1,5 +1,7 @@
 import React                                     from 'react'
-import { connect as reduxConnect }               from 'react-redux'
+import { createStore }                           from 'redux'
+import { connect as reduxConnect, Provider }     from 'react-redux'
+import reduxReducer                              from './../reducer'
 import { HashRouter as Router, Route, Link }     from 'react-router-dom'
 import { VictoryBar, VictoryPie }                from 'victory'
 import { Grid, Row, Col }                        from 'react-bootstrap'
@@ -7,6 +9,13 @@ import BaseCard                                  from './cards/BaseCard'
 
 // main component
 var CheshireCat = React.createClass({
+
+    componentWillMount: function(){
+
+        // initial state
+        var initialState = {}
+        this.store = createStore(reduxReducer, initialState)
+    },
 
     render: function(){
 
@@ -20,22 +29,23 @@ var CheshireCat = React.createClass({
         )
 
         return (
-            <div>
+            <Provider store={ this.store } >
                 <Router>
                     <div>
                         {navigationJSX}
                         {this.props.children}
                     </div>
                 </Router>
-            </div>
+            </Provider>
         )
     }
 
 })
 
 // redux mapping
-var mapStateToProps = function(state){
+/*var mapStateToProps = function(state){
     return {}
-}
+}*/
 
-module.exports = reduxConnect(mapStateToProps)(CheshireCat)
+//module.exports = reduxConnect(mapStateToProps)(CheshireCat)
+module.exports = CheshireCat
--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 import React                      from 'react'
 import ReactDOM                   from 'react-dom'
-import { createStore }            from 'redux'
-import { Provider }               from 'react-redux'
-import reduxReducer               from './reducer'
 import CheshireCat                from './components/CheshireCat'
 import Door                       from './components/Door'
 import BaseCard                   from './components/cards/BaseCard'
@@ -12,64 +9,58 @@ import { HashRouter as Router, Route, Link, hashHistory, browserHistory }  from 
 
 var launchApp = function() {
 
-    // initial state
-    var initialState = {}
-    var store = createStore(reduxReducer, initialState)
-
     ReactDOM.render(
-        <Provider store={ store } >
-            <CheshireCat>
-                <Door exact path="/">
-                    <BaseCard header="Welcome by the Cheshire Cat" />
-                </Door>
-                <Door path="/bar">
-                    <h1>Yeah</h1>
-                    <Door path="/bar/a">
+        <CheshireCat>
+            <Door exact path="/">
+                <BaseCard header="Welcome by the Cheshire Cat" />
+            </Door>
+            <Door path="/bar">
+                <h1>Yeah</h1>
+                <Door path="/bar/a">
+                    <BaseCard
+                        formSchema={{
+                            title: "Alice is lost",
+                            type: "object",
+                            properties: {
+                                title: {type: "string", title: "Title", default: "A new task"},
+                                done: {type: "boolean", title: "Done?", default: false}
+                            }
+                        }}
+                    >
+                        <VictoryLine/>
+                        <VictoryBar/>
                         <BaseCard
                             formSchema={{
-                                title: "Alice is lost",
+                                title: "Ouch",
                                 type: "object",
                                 properties: {
-                                    title: {type: "string", title: "Title", default: "A new task"},
-                                    done: {type: "boolean", title: "Done?", default: false}
+                                    date: {type: "string", format: "date", title: "Today?"}
                                 }
                             }}
                         >
-                            <VictoryLine/>
-                            <VictoryBar/>
-                            <BaseCard
-                                formSchema={{
-                                    title: "Ouch",
-                                    type: "object",
-                                    properties: {
-                                        date: {type: "string", format: "date", title: "Today?"}
-                                    }
-                                }}
-                            >
-                                Here is a clock
-                            </BaseCard>
+                            Here is a clock
                         </BaseCard>
-                    </Door>
-                    <Door path="/bar/b">
-                        <VictoryPie/>
-                    </Door>
+                    </BaseCard>
                 </Door>
-                <Door path="/pie">
-                <VictoryPie
-                    data={[
-                        {val:2,lab:"down"},
-                        {val:1,lab:"dunno"},
-                        {val:5,lab:"happy"}
-                    ]}
-                    x="lab"
-                    y={ (d)=> d.val }
-                    colorScale="qualitative"
-                    startAngle={-90}
-                    endAngle={90}
-                />
+                <Door path="/bar/b">
+                    <VictoryPie/>
                 </Door>
-            </CheshireCat>
-        </Provider>,
+            </Door>
+            <Door path="/pie">
+            <VictoryPie
+                data={[
+                    {val:2,lab:"down"},
+                    {val:1,lab:"dunno"},
+                    {val:5,lab:"happy"}
+                ]}
+                x="lab"
+                y={ (d)=> d.val }
+                colorScale="qualitative"
+                startAngle={-90}
+                endAngle={90}
+            />
+            </Door>
+        </CheshireCat>,
         document.getElementById('dashboard')
     )
 
